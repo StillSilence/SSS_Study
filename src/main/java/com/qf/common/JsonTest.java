4f145f6f235c510d58985e85bfc25e3caa517b9f
StillSilence <1642439136@qq.com>
2020-04-26 12:28:49 +0800

    小明提交的

--- src/main/java/com/qf/common/JsonTest.java
+++ src/main/java/com/qf/common/JsonTest.java
@@ -3,5 +3,6 @@ package com.qf.common;
 public class JsonTest {
     public static void main(String[] args) {
         System.out.println("新增内容!");
+        System.out.println("我有新增了一个内容！");
     }
 }
